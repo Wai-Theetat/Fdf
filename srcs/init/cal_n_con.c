b06ef0807cc5c0d4417496b642e0d13bc9770cb7
Theetat Dharmarungruang <tdharmar@z2t10c3.42bangkok.com>
2025-12-09 09:17:38 +0700

    s

--- srcs/init/cal_n_con.c
+++ srcs/init/cal_n_con.c
@@ -6,14 +6,24 @@
 /*   By: tdharmar <tdharmar@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/07 14:21:09 by tdharmar          #+#    #+#             */
-/*   Updated: 2025/12/07 14:30:26 by tdharmar         ###   ########.fr       */
+/*   Updated: 2025/12/09 09:13:42 by tdharmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-double	calc_x(int col, t_map *map)
+double	calc_x(int col_in, t_map *map)
 {
+	int	centre;
 
+	centre = (map->col - 1) * (map->spacing) / 2;
+	return ((double)col_in *(map->spacing) - centre);
+}
 
+double	calc_y(int row_in, t_map *map)
+{
+	int	centre;
+
+	centre = (map->row - 1) * (map->spacing) / 2;
+	return ((double)row_in *(map->spacing) - centre);
 }
